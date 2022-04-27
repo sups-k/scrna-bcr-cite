099da1bd3dd26e35944668e1eb25e2530d42edf6
Kumar <suk571@compute-e-16-229.o2.rc.hms.harvard.edu>
2022-04-27 14:17:50 -0400

    Changed file locations

--- BCR/03_filter_BCR_from_Seurat.R
+++ BCR/03_filter_BCR_from_Seurat.R
@@ -5,19 +5,19 @@ meta <- read.csv(file = "../../KW9079_Farmer/210624_A00873_0376_BHC3KTDSX2_KW907
   as_tibble()
 
 # Read functional makedb output
-h1 <- read_delim(file = "../../../../../../../../../../home/suk571/immcantation-python3.8.12/imm-env/makedb_output/functional/1_functional_parse-select.tab")
+h1 <- read_delim(file = "~/immcantation-python3.8.12/imm-env/makedb_output/AIRR/functional/1_functional_AIRR_parse-select.tab")
 
 # Subset metadata for sample read above & match column name of cell to makedb output
 h1_m <- meta %>%
   filter(patient_ID == "Hashtag1") %>%
-  rename(CELL = cells)
+  rename(cell_id = cells)
 
 # Merge both files
-merged_h1 <- merge(h1_m, h1, by = "CELL")
+merged_h1 <- merge(h1_m, h1, by = "cell_id")
 
 # Keep original column names
-merged_h1 <- merged_h1 %>% select(colnames(h1))
+merged_h1 <- merged_h1 %>% select(colnames(h1), integrated_snn_res.0.4)
 
 # Save to functional
-write_tsv(merged_h1, file = "../../../../../../../../../../home/suk571/immcantation-python3.8.12/imm-env/makedb_output/functional/filtered_from_Seurat/1_functional_parse-select.tab")
+write_tsv(merged_h1, file = "~/immcantation-python3.8.12/imm-env/makedb_output/AIRR/functional/filtered_from_Seurat/1_functional_parse-select.tab")
 rm(h1, h1_m, merged_h1)
